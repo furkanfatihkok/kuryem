cc5232a18641c741a4ea4ec4cd4111a06910136e
furkanfatihkok <furkankok296@gmail.com>
2026-02-19 13:44:53 +0300

    define app color palette and semantic colors add design system dimensions and spacing constants

---
+++ Presentation/Common/Styles/Dimens.swift
@@ -0,0 +1,64 @@
+//
+//  Dimens.swift
+//  kuryem
+//
+//  Created by FFK on 19.02.2026.
+//
+
+import UIKit
+
+struct Dimens {
+    // MARK: - Spacing
+    static let spacingXSmall: CGFloat = 4
+    static let spacingSmall: CGFloat = 8
+    static let spacingMedium: CGFloat = 16
+    static let spacingLarge: CGFloat = 24
+    static let spacingXLarge: CGFloat = 32
+    static let spacingXXLarge: CGFloat = 48
+    
+    // MARK: - Padding
+    static let paddingHorizontal: CGFloat = 24
+    static let paddingVertical: CGFloat = 16
+    
+    // MARK: - Corner Radius
+    static let cornerRadiusSmall: CGFloat = 8
+    static let cornerRadiusMedium: CGFloat = 12
+    static let cornerRadiusLarge: CGFloat = 16
+    static let cornerRadiusXLarge: CGFloat = 24
+    
+    // MARK: - Button
+    static let buttonHeight: CGFloat = 52
+    static let buttonCornerRadius: CGFloat = 12
+    
+    // MARK: - TextField
+    static let textFieldHeight: CGFloat = 52
+    static let textFieldCornerRadius: CGFloat = 12
+    static let textFieldBorderWidth: CGFloat = 1
+    
+    // MARK: - Icon
+    static let iconSmall: CGFloat = 20
+    static let iconMedium: CGFloat = 24
+    static let iconLarge: CGFloat = 32
+    
+    // MARK: - Onboarding
+    static let onboardingImageHeight: CGFloat = 280
+    static let onboardingIndicatorSize: CGFloat = 8
+    static let onboardingIndicatorSpacing: CGFloat = 8
+    
+    // MARK: - Code Input
+    static let codeInputBoxSize: CGFloat = 48
+    static let codeInputSpacing: CGFloat = 12
+    
+    // MARK: - Font Sizes
+    static let fontSizeXSmall: CGFloat = 12
+    static let fontSizeSmall: CGFloat = 14
+    static let fontSizeMedium: CGFloat = 16
+    static let fontSizeLarge: CGFloat = 18
+    static let fontSizeXLarge: CGFloat = 24
+    static let fontSizeXXLarge: CGFloat = 28
+    
+    // MARK: - Border Width
+    static let borderWidthThin: CGFloat = 1
+    static let borderWidthMedium: CGFloat = 2
+}
+
